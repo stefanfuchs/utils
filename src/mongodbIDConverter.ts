7ce681bf4ccd2abbed111a1468d6d5fb131d452b
Stefan <stefan1234@gmail.com>
2019-06-12 00:56:42 -0300

    Add typescript typings for mongoDb id converter functions.

--- src/mongodbIDConverter.ts
+++ src/mongodbIDConverter.ts
@@ -1,14 +1,10 @@
 
 /** Replaces id with _id in the object. Returns a new object with the field replaced */
-export function convertIdToMongoDb(obj: { id?: any, [key: string]: any }): any {
-  const obj2 = { ...obj, _id: obj.id }
-  delete obj2.id
-  return obj2
+export function convertIdToMongoDb<T extends { id: any, _id?: undefined, [key: string]: any }>(obj: T): T & { _id: T['_id'], id: undefined } {
+  return { ...obj, _id: obj.id, id: undefined }
 }
 
 /** Replaces _id with id in the object. Returns a new object with the field replaced */
-export function convertIdFromMongoDb(obj: { _id?: any, [key: string]: any }): any {
-  const obj2 = { ...obj, id: obj._id }
-  delete obj2._id
-  return obj2
+export function convertIdFromMongoDb<T extends { _id: any, id?: undefined, [key: string]: any }>(obj: T): T & { id: T['_id'], _id: undefined } {
+  return { ...obj, id: obj._id, _id: undefined }
 }
