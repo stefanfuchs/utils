1ba096dfb405a3f5f3e3f757c1f7aa34f54efcd6
Stefan <stefan1234@gmail.com>
2019-06-12 01:05:53 -0300

    Set id as optional

--- src/mongodbIDConverter.ts
+++ src/mongodbIDConverter.ts
@@ -1,10 +1,10 @@
 
 /** Replaces id with _id in the object. Returns a new object with the field replaced */
-export function convertIdToMongoDb<T extends { id: any, _id?: undefined, [key: string]: any }>(obj: T): T & { _id: T['_id'], id: undefined } {
+export function convertIdToMongoDb<T extends { id?: any, _id?: undefined, [key: string]: any }>(obj: T): T & { _id: T['_id'], id: undefined } {
   return { ...obj, _id: obj.id, id: undefined }
 }
 
 /** Replaces _id with id in the object. Returns a new object with the field replaced */
-export function convertIdFromMongoDb<T extends { _id: any, id?: undefined, [key: string]: any }>(obj: T): T & { id: T['_id'], _id: undefined } {
+export function convertIdFromMongoDb<T extends { _id?: any, id?: undefined, [key: string]: any }>(obj: T): T & { id: T['_id'], _id: undefined } {
   return { ...obj, id: obj._id, _id: undefined }
 }
